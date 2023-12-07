5513753dc135090cb53b8c8465e4008a71cb149c
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-12-07 22:48:41 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./Components/style.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import "./index.css";
+import Delivery from "./Components/Delivery";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Delivery />
     </>
   );
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,13 @@
-import Hero1 from "../assets/Images/hero1.png";
-import Cert from "../assets/Images/certi.png";
+import HeroImage from "../assets/Images/hero1.png";
+import Certificate from "../assets/Images/certi.png";
 
 const Hero = () => {
   return (
     <div className="container-fluid col-xxl-8 px-4 py-5 hero-container">
-      <div className="row flex-lg-row-reverse align-items-center g-5 ">
+      <div className="row flex-lg-row-reverse align-items-center g-5">
         <div className="col-10 col-sm-8 col-lg-6">
           <img
-            src={Hero1}
+            src={HeroImage}
             className="d-block mx-lg-auto img-fluid"
             alt="Bootstrap Themes"
             width="700"
@@ -16,31 +16,29 @@ const Hero = () => {
           />
         </div>
         <div className="col-lg-6 py-5 hero-text">
-          <h1 className="display-1 fw-bold text-body-emphasis lh-1 mb-3">
-            <span>
-              Fresh Foods <br /> For You
-            </span>
+          <h1 className="display-1 fw-bold lh-2 mb-3">
+            <span>Your Fresh Oil Shop</span>
           </h1>
           <div className="d-flex align-items-center my-5">
-            <img src={Cert} alt="" className="me-4" />
+            <img src={Certificate} alt="" className="me-3" />
             <p className="fw-bold">
-              We give 100% organic & fresh foods for you.
+              We make 100% organic & fresh products
               <br />
-              Lets eat and live healthy...!
+              Lets use healthy products...!
             </p>
           </div>
           <div className="d-grid gap-2 d-md-flex justify-content-md-start">
             <button
               type="button"
-              className="btn btn-primary btn-lg px-4 me-md-2"
+              className="btn btn-warning btn-lg px-4 me-md-2 rounded-0"
             >
-              Primary
+              About Us
             </button>
             <button
               type="button"
-              className="btn btn-outline-secondary btn-lg px-4"
+              className="btn btn-light btn-lg px-4 rounded-0"
             >
-              Default
+              Our Products
             </button>
           </div>
         </div>
